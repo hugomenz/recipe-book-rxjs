b128c49e8146bb993eec8df177bf4c89423005e2
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-10 19:08:47 +0100

    Added PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
+import { DataViewModule } from 'primeng/dataview';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecipesListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RecipesListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, DataViewModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
